bd61e7c9333f8617ea345f87fdc93e6da06fc7c5
yajium <yajiuma720@gmail.com>
2023-08-24 00:15:42 +0900

    feat: add tailwind

---
+++ packages/vite/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./src/emoji-picker/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
