8aa445c5f880fee088554320d91a29a384acf0ca
yajium <yajiuma720@gmail.com>
2023-08-27 13:04:52 +0900

    fix

--- packages/vite/src/App.tsx
+++ packages/vite/src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Picker from "./emoji-picker/components/picker/Picker";
+import { Emoji } from "./emoji-picker/type";
 
 function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-orange-100">
-      <Picker isShow={true} />
+      <Picker
+        isShow={true}
+        onEmojiSelect={(selectedEmoji: Emoji) => console.log(selectedEmoji)}
+      />
     </main>
   );
 }
