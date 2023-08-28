3373f712240254835889fc6b002838b8d5ad44a8
yajium <chihiro.yajima@compass-e.com>
2023-08-28 19:41:29 +0900

    fix: vite projects

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,14 +1,14 @@
+import { EmojiType, Picker } from "ms-3d-emoji-picker";
+import "ms-3d-emoji-picker/styles/index.css";
 import React from "react";
 import "./App.css";
-import { Picker } from "./emoji-picker/components/picker";
-import { Emoji } from "./emoji-picker/type";
 
 function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-orange-100">
       <Picker
         isOpen={true}
-        onEmojiSelect={(selectedEmoji: Emoji) => console.log(selectedEmoji)}
+        onEmojiSelect={(selectedEmoji: EmojiType) => console.log(selectedEmoji)}
       />
     </main>
   );
