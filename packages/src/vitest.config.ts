6d48a7551f973e51f2e4ef00e6b8b80213f4f765
yajium <yajiuma720@gmail.com>
2023-09-05 22:33:42 +0900

    clean: change test setting

--- packages/src/vitest.config.ts
+++ packages/src/vitest.config.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/__tests__/vitest.setup.ts",
+    setupFiles: "./__tests__/vitest.setup.ts",
   },
 });
--- packages/tailwind.config.js
+++ packages/tailwind.config.js
@@ -3,23 +3,7 @@
 module.exports = {
   content: ["./src/components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      translate: {
-        t0: "0",
-        t1: "2.4rem",
-        t2: "4.7rem",
-        t3: "7rem",
-        t4: "9.3rem",
-        t5: "11.6rem",
-        t6: "13.9rem",
-        t7: "16.2rem",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
